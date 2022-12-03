37d2a9c8eb18cfb6671c6bd32ef7b994fd6f2ec0
NicolaWidmer <widmenic@student.ethz.ch>
2022-12-03 11:23:21 +0100

    solved day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,13 +1,64 @@
-fn main() {
-    let vec : Vec<i64> = include_str!("in.txt").split('\n')
-    .map(str::parse::<i64>)
-    .map(Result::unwrap)
-    .collect();
-    
-    let ans1 : i64 = vec.iter().sum();
-    let ans2 : i64 = vec.iter().sum();
+use std::collections::{HashSet,HashMap};
 
-    println!("The ans for part 1 is {}",ans1);
-    println!("The ans for part 2 is {}",ans2);
+fn main() {
+
+    let mut points = HashMap::new();
+    for i in 0..26{
+        points.insert('a' as i64 + i,i+1);
+        points.insert('A' as i64 + i,i+27);
+    }
+
+    println!("The ans for part 1 is {}",solve1(&points));
+    println!("The ans for part 2 is {}",solve2(&points));
+}
+
+fn solve2(points:&HashMap<i64,i64>) -> i64{
+    let mut ans:i64 = 0;
+
+    let vec: Vec<&str>  = include_str!("in.txt").split('\n')
+    .collect();
+
+    for i in 0..(vec.len()/3){
+        
+        let s1:HashSet<char> = HashSet::from_iter(vec[3*i].chars());
+        let s2:HashSet<char> = HashSet::from_iter(vec[3*i+1].chars());
+
+        for c in vec[3*i+2].chars(){
+            if s1.contains(&c)&&s2.contains(&c){
+                ans+=points.get(&(c as i64)).unwrap();
+                break;
+            }
+        }
+
+    }
+
+    return ans;
+}
+
+fn solve1(points:&HashMap<i64,i64>) -> i64{
+    let mut ans:i64 = 0;
+
+    let vec: Vec<(&str,&str)>  = include_str!("in.txt").split('\n')
+    .map(|x| {x.split_at(x.len()/2)})
+    .collect();
+
+    for (fst,snd) in vec{
+
+        let mut curs=HashSet::new();
+
+        for c in fst.chars(){
+            curs.insert(c);
+        }
+
+        for c in snd.chars(){
+            if curs.contains(&c){
+                ans+=points.get(&(c as i64)).unwrap();
+                break;
+            }
+        }
+
+    }
+
+    return ans;
 }
 
